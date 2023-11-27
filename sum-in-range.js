481ded9b5220076df599c12bbacbde70924603b8
JC2295 <64856330+JC2295@users.noreply.github.com>
2023-11-27 14:48:04 +0000

    Challenge Complete

--- sum-in-range.js
+++ sum-in-range.js
@@ -1,1 +1,29 @@
+function sumAll(arr) {
 
+  let sum = arr[0] + arr[1];
+
+  let rangeArr = [];
+
+  if (arr[0] > arr[1]) {
+    for (let i = arr[1] + 1; i < arr[0]; i++) {
+      rangeArr.push(i);
+    }
+  }
+  else if (arr[1] > arr[0]) {
+    for (let i = arr[0] + 1; i < arr[1]; i++) {
+      rangeArr.push(i);
+    }
+  }
+
+  let rangeSum = 0;
+
+  for (let i = 0; i < rangeArr.length; i++) {
+    rangeSum += rangeArr[i];
+  }
+
+  return rangeSum + sum;
+}
+
+console.log(sumAll([1, 4]));
+
+// Challenge Complete
